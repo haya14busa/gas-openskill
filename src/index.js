e594092594a865dcbad351a1af236c7499edee12
haya14busa <haya14busa@gmail.com>
2022-02-16 15:31:48 +0000

    tweak exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-const openskill = require('openskill')
-global.rating = openskill.rating;
-global.rate = openskill.rate;
-global.ordinal = openskill.ordinal;
-global.openskill = openskill;
+global.openskill = require('openskill');
